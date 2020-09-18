bb52c5c1c072938ca6699d264471286472f774e2
Manesh Harish Ramji <maneshharish@gmail.com>
2020-09-18 14:38:58 +1200

    Add project files.

---
+++ Repeating Elements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Repeating_Elements
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            int[] arr = { 4, 2, 4, 5, 2, 3, 1 };
+            
+            int[] count = new int[arr.Length];
+            int i;
+
+            Console.Write("Repeated elements are: ");
+            for (i = 0; i < arr.Length; i++)
+            {
+                if (count[arr[i]] == 1)
+                    Console.Write(arr[i] + " ");
+                else
+                    count[arr[i]]++;
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
